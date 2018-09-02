c7e512098d44006fcabfa1ec3523cea29805e54a
Kiara Atkins <atkinsk98@gmail.com>
2018-09-02 19:15:44 -0400

    Creates a new scorecard for each user

--- project_files/yahtzee_game/game_manager.py
+++ project_files/yahtzee_game/game_manager.py
@@ -1,10 +1,21 @@
+from project_files.yahtzee_game.yahtzee_scorecard import Scorecard
+
+
 class GameManager():
 
-    def __init__(self, UI, dice_roller):
+    def __init__(self, UI, turn_manager):
         self.ui = UI
-        self.dice_roller = dice_roller
+        self.turn_manager = turn_manager
+        self.scorecards = []
 
     def start_game(self):
+        self.turn_manager.take_a_turn()
+
+        number_of_players = self.ui.ask_user("How many people are playing?")
+        while number_of_players != 0:
+            self.scorecards.append(Scorecard())
+            number_of_players -= 1
+
         self.ui.display("Welcome to YAHTZEE!!!!!")
-        dice_roll = self.dice_roller.roll_dice()
-        self.ui.display("Dice roll: " + str(dice_roll))
+
+#TODO: Make tests so that this ridiculous order of doing things doesn't work.
--- test_files/game_manager_test.py
+++ test_files/game_manager_test.py
@@ -1,50 +1,89 @@
 import unittest
 
 from project_files.yahtzee_game.game_manager import GameManager
+from project_files.yahtzee_game.yahtzee_scorecard import Scorecard
 
 
 class MockUI:
 
     def __init__(self):
         self.list_of_displayed_messages = []
+        self.list_of_questions= []
+        self.user_response = None
 
     def display(self, message):
         self.list_of_displayed_messages.append(message)
 
-    def check_if_method_was_displayed(self, message):
+    def check_if_message_was_displayed(self, message):
         if message in self.list_of_displayed_messages:
             return True
         else:
             return False
 
+    def ask_user(self, message):
+        self.list_of_questions.append(message)
+        return self.user_response
 
-class MockDiceRoller:
+    def set_test_response(self, response):
+        self.user_response = response
+
+    def last_question_to_user(self):
+        return self.list_of_questions[-1]
+
+
+class MockTurnManager:
 
     def __init__(self):
-        self.dice_to_return = None
+        self.take_a_turn_was_called = False
 
-    def set_up_the_mock_to_return_these_dice(self, dice):
-        self.dice_to_return = dice
+    def was_take_a_turn_called(self):
+        return self.take_a_turn_was_called
 
-    def roll_dice(self):
-        return self.dice_to_return
+    def take_a_turn(self):
+        self.take_a_turn_was_called = True
 
 
 class GameManagerTest(unittest.TestCase):
 
     def setup_method(self, method):
         self.ui = MockUI()
-        self.dice_roller = MockDiceRoller()
-        self.game_manager = GameManager(UI=self.ui, dice_roller=self.dice_roller)
+        self.turn_manager = MockTurnManager()
+        self.ui.set_test_response(0)
+        self.game_manager = GameManager(UI=self.ui, turn_manager=self.turn_manager)
 
     def test_welcome_message(self):
         self.game_manager.start_game()
-        self.assertTrue(self.ui.check_if_method_was_displayed("Welcome to YAHTZEE!!!!!"))
+        self.assertTrue(self.ui.check_if_message_was_displayed("Welcome to YAHTZEE!!!!!"))
 
-    def test_rolls_the_dice(self):
-        self.dice_roller.set_up_the_mock_to_return_these_dice([2,2,4,5,6])
+    def test_takes_a_turn(self):
         self.game_manager.start_game()
-        self.assertTrue(self.ui.check_if_method_was_displayed("Dice roll: [2, 2, 4, 5, 6]"))
+        self.assertTrue(self.turn_manager.was_take_a_turn_called())
+
+    def test_asks_for_number_of_users_and_creates_a_scorecard_for_each(self):
+        self.ui.set_test_response(2)
+
+        self.game_manager.start_game()
+
+        self.assertEqual(self.ui.last_question_to_user(), "How many people are playing?")
+        self.assertTrue(len(self.game_manager.scorecards) == 2)
+        for scorecard in self.game_manager.scorecards:
+            self.assertIsInstance(scorecard, Scorecard)
+
+        self.assertNotEquals(self.game_manager.scorecards[0], self.game_manager.scorecards[1])
+
+    def test_asks_for_number_of_users_with_3_users(self):
+        self.ui.set_test_response(3)
+
+        self.game_manager.start_game()
+
+        self.assertEqual(self.ui.last_question_to_user(), "How many people are playing?")
+        self.assertTrue(len(self.game_manager.scorecards) == 3)
+        for scorecard in self.game_manager.scorecards:
+            self.assertIsInstance(scorecard, Scorecard)
+
+        self.assertNotEquals(self.game_manager.scorecards[0], self.game_manager.scorecards[1])
+        self.assertNotEquals(self.game_manager.scorecards[0], self.game_manager.scorecards[2])
+        self.assertNotEquals(self.game_manager.scorecards[1], self.game_manager.scorecards[2])
 
 
 # TODO: divide game manager into turn manager that manages a single turn for a single player
